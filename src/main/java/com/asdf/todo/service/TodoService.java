4af47be747f5017fef3e4275347d7e945cd08bdb
haeuniea <haeunhaa@gmail.com>
2026-08-13 10:44:29 +0900

    feat: Todo 서비스 구현

--- src/main/java/com/asdf/todo/service/TodoService.java
+++ src/main/java/com/asdf/todo/service/TodoService.java
@@ -1,3 +1,40 @@
 package com.asdf.todo.service;
 
-public class TodoService {}
+import com.asdf.todo.model.Todo;
+import com.asdf.todo.repository.TodoInMemoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+// Todo 리스트를 관리하는 서비스 빈
+@Service
+public class TodoService {
+    private final TodoInMemoryRepository todoRepository;
+
+    @Autowired
+    public TodoService(TodoInMemoryRepository todoRepository) {
+        this.todoRepository = todoRepository;
+    }
+
+    public List<Todo> findAll() {
+        return todoRepository.findAll();
+    }
+
+    public Todo findById(Long id) {
+        return todoRepository.findById(id);
+    }
+
+    public Todo save(Todo todo) {
+        return todoRepository.save(todo);
+    }
+
+    public Todo update(Long id, Todo todo) {
+        todo.setId(id);
+        return todoRepository.save(todo);
+    }
+
+    public void delete(Long id) {
+        todoRepository.deleteById(id);
+    }
+}
