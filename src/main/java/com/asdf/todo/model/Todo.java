acb3cc32eadaa597bf48ddeb025b476c1027513a
haeuniea <haeunhaa@gmail.com>
2026-08-13 09:49:56 +0900

    feat: Todo 모델 구현

--- src/main/java/com/asdf/todo/model/Todo.java
+++ src/main/java/com/asdf/todo/model/Todo.java
@@ -1,3 +1,18 @@
 package com.asdf.todo.model;
 
-public class Todo {}
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+// 모델 클래스
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Todo {
+    private Long id;
+    @NonNull
+    private String title;
+    private String description;
+    private boolean completed;
+}
